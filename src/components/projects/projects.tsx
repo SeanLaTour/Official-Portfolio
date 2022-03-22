8dc0e58379fe6767e6bdfe8bd3a02d4e92f2eebb
Sean La Tourette <sean.latourette@straightfire.tech>
2022-03-21 18:50:10 -0700

    laptop page looking good

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -10,9 +10,45 @@ import {
 } from "../shared/portfolio_text";
 import { BiFileBlank } from "react-icons/bi";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Projects: React.FC = (props) => {
+  const [selectedProject, setSelectedProject] = useState({})
   const [toggleLaptopScreen, setToggleLaptopScreen] = useState(false);
+
+  const setProject = (e) => {
+    console.log(e.target.id)
+    switch (e.target.id) {
+      case "Nesh":
+        return  setSelectedProject({title: "NESH",
+          description: "Nesh is a mic to pdf guitar tabliture app. It can listen to the acoustic tones of your guitar and translate it into PDFs while simutaniously allowing users to create and edit tabs from scratch. It was created using React, WebAudioDaw, and React-PDF.",
+          link: ""
+        })
+        case "Alaeris":
+          return  setSelectedProject({title: "Alaeris",
+            description: "",
+            link: ""
+          })
+          case "Zipti":
+            return  setSelectedProject({title: "",
+              description: "",
+              link: ""
+            })
+            case "WWC":
+              return  setSelectedProject({title: "",
+                description: "",
+                link: ""
+              })
+              case "3D":
+                return  setSelectedProject({title: "",
+                  description: "",
+                  link: ""
+                })
+    }
+  }
+
+  useEffect(() =>{},[selectedProject])
+
   return (
     <Box>
       <Image
@@ -61,27 +97,25 @@ const Projects: React.FC = (props) => {
           display={toggleLaptopScreen ? "flex" : "none"}
           flexDirection="column"
         >
-          <Box display="flex" flexDirection="row">
-          <PortfolioTextStandard
-            onClick={() => setToggleLaptopScreen(false)}
-            fontFamily="Wallpoet"
-          >
-            {"<-"}
-          </PortfolioTextStandard>
-          <PortfolioTextStandard fontFamily="Wallpoet">
-            NESH: Visit
-          </PortfolioTextStandard>
-          <PortfolioTextStandard lineHeight="120%" fontFamily="Wallpoet">
-            Nesh is a mic to pdf guitar tabliture app.
-          </PortfolioTextStandard>
+          <Box display="flex" flexDirection="column">
+            <PortfolioTextStandard
+              onClick={() => setToggleLaptopScreen(false)}
+              fontFamily="Wallpoet"
+            >
+              {"<-"} {selectedProject.title}:
+            </PortfolioTextStandard>
+            <PortfolioTextStandard
+              width="33vw"
+              lineHeight="120%"
+              fontFamily="Wallpoet"
+            >
+              {selectedProject.description}
+             </PortfolioTextStandard>
+            <PortfolioTextStandard fontFamily="Wallpoet">Visit: <a href={selectedProject.link} target="_blank">{selectedProject.title} -></a></PortfolioTextStandard>
           </Box>
-
-          <PortfolioTextStandard fontFamily="Wallpoet">
-            Tech:
-          </PortfolioTextStandard>
-          <PortfolioTextStandard fontFamily="Wallpoet">
-            React, WebAudioDaw, JavaScript, CSS
-          </PortfolioTextStandard>
+        </Box>
+        <Box position="relative" left="0" top="22%">
+          {" "}
         </Box>
         <Grid
           width="8rem"
@@ -94,22 +128,32 @@ const Projects: React.FC = (props) => {
           display={toggleLaptopScreen ? "none" : "grid"}
         >
           <HStack
-            onClick={() => {
+            id="Nesh"
+            onClick={(e) => {
+              setProject(e)
               setToggleLaptopScreen(true);
+
+              console.log(selectedProject)
             }}
           >
             <BiFileBlank />
-            <PortfolioTextStandard fontFamily="Wallpoet">
+            <PortfolioTextStandard  id="Nesh"   fontFamily="Wallpoet">
               Nesh
             </PortfolioTextStandard>
           </HStack>
-          <HStack
+          <HStack id="Alaeris"
+                    onClick={(e) => {
+                      setProject(e)
+                      setToggleLaptopScreen(true);
+        
+                      console.log(selectedProject)
+                    }}
             onClick={() => {
               setToggleLaptopScreen(true);
             }}
           >
             <BiFileBlank />
-            <PortfolioTextStandard fontFamily="Wallpoet">
+            <PortfolioTextStandard id="Alaeris" fontFamily="Wallpoet">
               Alaeris
             </PortfolioTextStandard>
           </HStack>
